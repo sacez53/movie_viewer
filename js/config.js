51957e1c7fb28a1ffde1a595ed2308bce2a0ae37
Sacha GUITTER <sachaguitter@gmail.com>
2026-06-18 18:23:13 +0200

    Update TMDB bearer token in config
    Replace the TMDB_BEARER_TOKEN value in js/config.js with a new JWT bearer token used for TMDB API authentication, replacing the previous token.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Renommez ce fichier en "config.js" et remplissez vos clés
 export const CONFIG = {
     // Clé ou Token TMDB (Bearer) - Récupérable sur https://www.themoviedb.org/settings/api
-    TMDB_BEARER_TOKEN: '76352476f338d9928dacc7e08f6f49e2',
+    TMDB_BEARER_TOKEN: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjM1MjQ3NmYzMzhkOTkyOGRhY2M3ZTA4ZjZmNDllMiIsIm5iZiI6MTc4MTc5NzYzNy43MzIsInN1YiI6IjZhMzQxMzA1MTk5YzdlOTYxOTg1MWI0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aX4tAg_aS2H-rj0mNPyom1Bcu-sCq5rhR2SOqMjPzS8',
 
     // Configuration Firebase - Récupérable sur la console Firebase (Project Settings)
     FIREBASE: {
